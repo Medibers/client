085221dfc4d16da597d9bdfbae2d1f18806262e1
Jerry Edebua <jerry.edebua.01@gmail.com>
2022-07-21 06:12:50 +0300

    Re-arrange item details

--- src/views/Item/Item.tsx
+++ src/views/Item/Item.tsx
@@ -31,23 +31,33 @@ const Item: React.FC<IItem> = ({ result, selectedItems }) => {
           <ImageSlider urls={images} />
         </div>
         <div className="ion-padding">
-          <AddToCartButton selectedItems={selectedItems} result={result} />
           <EditButton result={result} />
           {/* <ContentHeader message={`Supplied by ${result.pharmacy.name}`} /> */}
           <IonList lines="none">
-            <IonItem className="ion-no-padding">
-              <IonLabel className="wrap">
+            <IonItem
+              className="ion-no-padding"
+              style={{ '--min-height': 'unset' }}
+            >
+              <IonLabel className="ion-no-margin wrap">
                 <h1>{result.item.name}</h1>
+                <h4>
+                  <i className="ion-label-secondary">
+                    {getItemState(available)}
+                  </i>
+                </h4>
               </IonLabel>
             </IonItem>
-            <IonItem className="ion-no-padding">
+            <IonItem
+              className="ion-no-padding"
+              style={{ '--min-height': 'unset' }}
+            >
               <IonLabel className="no-wrap">
-                <h2>
-                  <b>{formatMoney(price)}</b>
-                </h2>
-                <h4>{getItemState(available)}</h4>
+                <h2 style={{ fontWeight: 500 }}>{formatMoney(price)}</h2>
               </IonLabel>
             </IonItem>
+            {available && (
+              <AddToCartButton selectedItems={selectedItems} result={result} />
+            )}
             <ListedDetails details={item.specification} />
           </IonList>
         </div>
--- src/views/Item/ListedDetails.tsx
+++ src/views/Item/ListedDetails.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
-import { IonLabel } from '@ionic/react'
+import { IonItemDivider, IonLabel } from '@ionic/react'
 
 import { ListItem } from 'components'
 
 const ListedDetails: React.FC<{ details?: string[] }> = ({ details }) => {
   return details && details.length ? (
-    <ListItem className="ion-no-padding" lines="none">
-      <IonLabel className="wrap">
-        {details.map(o => (
-          <h4 key={o}>{o}</h4>
-        ))}
-      </IonLabel>
-    </ListItem>
+    <React.Fragment>
+      <IonItemDivider style={{ minHeight: '1.2rem', marginBottom: '0.6rem' }} />
+      <ListItem className="ion-no-padding" lines="none">
+        <IonLabel className="ion-no-margin wrap">
+          {details.map(o => (
+            <h4 key={o}>{o}</h4>
+          ))}
+        </IonLabel>
+      </ListItem>
+    </React.Fragment>
   ) : null
 }
 
