36f6159a5790bd73cadcf6208c50e4f7ebb3688b
Jerry Edebua <jerry.edebua.01@gmail.com>
2022-07-21 05:27:02 +0300

    Make Order button bigger

--- src/views/Order/Order.tsx
+++ src/views/Order/Order.tsx
@@ -225,6 +225,7 @@ const Component: React.FC<IOrderProps> = props => {
                 onClick={onPrimaryAction}
                 disabled={locationNotAvailable}
                 className="ion-margin-top ion-action-primary"
+                size="default"
               >
                 {primaryAction}
               </IonButton>
